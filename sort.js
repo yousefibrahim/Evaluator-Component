ffacb1115b01845e022b3ffe190e0916ec7c09b7
IbrahimZamly <45468074+IbrahimZamly@users.noreply.github.com>
2019-04-07 19:47:14 +0200

    Sort function by JS

---
+++ sort.js
@@ -0,0 +1,30 @@
+var data = [{
+	user:'ahmed',
+	score:44
+},{
+	user:'lol',
+	score:7
+},{
+	user:'5od',
+	score:10
+},{
+	user:'hat',
+	score:2
+	
+	
+	
+	
+}];
+
+function compare(a,b){
+	return a.score-b.score;
+	
+
+	}
+consolce.log(data);	
+data.sort(compare);
+
+	
+
+
+
